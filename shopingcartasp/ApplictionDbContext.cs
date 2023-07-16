45d26844852eb020677a0a6b1d87c4f87d34d29e
Rayhan Morshed <rayhanm120@gmail.com.com>
2023-07-16 16:33:17 +0600

    wqeqweretest4r

---
+++ shopingcartasp/ApplictionDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Shopingcut.Models;
+
+public class ApplicationDbContext:IdentityDbContext
+{
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+}
